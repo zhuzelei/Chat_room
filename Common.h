f7023025794b1f0781cfa8464188bdcc79837823
NameZZL <31604203+zhuzelei@users.noreply.github.com>
2019-04-10 20:14:21 +0800

    Update Common.h

--- Common.h
+++ Common.h
@@ -21,7 +21,6 @@
 // 服务器端口号
 #define SERVER_PORT 8888
  
-// int epoll_create(int size)中的size
 // 为epoll支持的最大句柄数
 #define EPOLL_SIZE 5000
  
@@ -53,8 +52,6 @@ static void addfd( int epollfd, int fd, bool enable_et )
         ev.events = EPOLLIN | EPOLLET;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev);
     // 设置socket为非阻塞模式
-    // 套接字立刻返回，不管I/O是否完成，该函数所在的线程会继续运行
-    //eg. 在recv(fd...)时，该函数立刻返回，在返回时，内核数据还没准备好会返回WSAEWOULDBLOCK错误代码
     fcntl(fd, F_SETFL, fcntl(fd, F_GETFD, 0)| O_NONBLOCK);
     printf("fd added to epoll!\n\n");
 }
